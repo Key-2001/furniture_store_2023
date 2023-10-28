09b399e4300adf277aa4dded262881d0efcdc333
keyl1yy <keyly.la@fordeer.io>
2023-10-28 14:45:47 +0700

    update config code

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Fragment, useCallback, useState } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Sidebar from "../components/Sidebar/Sidebar";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 const Layout = ({ children, isLayout }) => {
   //! State
   const [isShowSidebar, setIsShowSidebar] = useState(false);
@@ -15,10 +14,6 @@ const Layout = ({ children, isLayout }) => {
   //! Render
   return (
     <Fragment>
-      <MessengerCustomerChat
-        pageId="157771114084700"
-        appId="1341663279776099"
-      />
       {isLayout && (
         <Fragment>
           <Header handleToggleShowSidebar={handleToggleShowSidebar} />
--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,15 @@ import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { CartContextProvider } from "./context/CartContext.jsx";
 import { DiscountContextProvider } from "./context/DiscountContext.jsx";
 import { AdminContextProvider } from "./context/AdminContext.jsx";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* <MessengerCustomerChat
+        pageId="157771114084700"
+        appId="1341663279776099"
+      /> */}
       <AuthContextProvider>
         <CartContextProvider>
           <DiscountContextProvider>
